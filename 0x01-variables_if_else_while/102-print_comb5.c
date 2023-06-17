497711656248630b8b142c5e33f67fd6a29514b9
bubuluma <rotondwa.bubuluma@gmail.com>
2023-06-17 10:08:28 -0700

    printb comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,7 +9,6 @@
 int main(void)
 {
 	int num1, num2;
-
 	for (num1 = 0; num1 <= 98; num1++)
 	{
 		for (num2 = num1 + 1; num2 <= 99; num2++)
@@ -19,16 +18,12 @@ int main(void)
 			putchar(' ');
 			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
-
 			if (num1 == 98 && num2 == 99)
 				continue;
-
 			putchar(',');
 			putchar(' ');
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
