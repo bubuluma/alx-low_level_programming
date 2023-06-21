839b35469ade771ba79722f4c63a2eaa4d9020f3
bubuluma <rotondwa.bubuluma@gmail.com>
2023-06-21 06:09:55 -0700

    islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,23 +1,16 @@
 #include "main.h"
-/**
-* print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
-* followed by a new line
-*/
-void print_alphabet_x10(void)
-{
-char ch;
-int i;
 
-i = 0;
-while (i < 10)
+/**
+* _islower - check if char is lowercase
+* @c: is the char to be checked
+* Return: 1 if char is lowercase, otherwise 0.
+*/
+
+int _islower(int c)
 {
-ch = 'a';
-while (ch <= 'z')
-{
-_putchar(ch);
-ch++;
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
 }
-_putchar('\n');
-i++;
-}
-}
+
